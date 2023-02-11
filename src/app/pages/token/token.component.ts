b3dc647feb501a264177e62d2553aa62a14d6117
cristianizzo <cristianizzo@me.com>
2023-02-11 23:26:23 +0100

    init

---
+++ src/app/pages/token/token.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-token',
+  templateUrl: './token.component.html',
+  styleUrls: ['./token.component.scss']
+})
+export class PageTokenComponent {
+
+}
